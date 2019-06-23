b0e2c8a37479a55fa6f523924ecf8cd8f169ea31
Paris Cornett <pariscornett@gmail.com>
2019-06-22 22:34:09 -0500

    added code to empty form upon clicking submit :matrix:

--- assets/tip.js
+++ assets/tip.js
@@ -7,26 +7,27 @@ let calculatedTotal;
 let calculatedSplit;
 
 
-
-
-
 //get input from form and store data in corresponding variables
 $("#submit").on("click" ,function(){
     event.preventDefault();
     let billInput = $("#bill-input").val().trim();
     let tipInput = $("#tip-input").val().trim()/100; //divided by 100 to turn tip input into percentage
     let splitInput = $("#split-input").val().trim();
+    $("#bill-input").val("");
+    $("#tip-input").val("");
+    $("#split-input").val("");
     console.log(billInput);
     console.log(tipInput);
     console.log(splitInput);
     calculate();
-    
+   
 });
 
 //function to calculate tip
-function calculate(){ 
-   //multiply the total bill by the tip amount
-    //the total is the tip amount, store this in a variable
+function calculate(str){ 
+   //multiply the total bill by the tip amount,the total is the tip amount, store this in a variable
+   let calculatedTip = Number.parseFloat(billInput)* Number.parseFloat(tipInput);
+   console.log("This is the calculated tip: " + calculatedTip);
     //append the tip amount to the corresponding div
     //take the total bill amount and add the tip amount
     //the total is the total amount, store this in a variable
@@ -36,4 +37,3 @@ function calculate(){
         //this is the amount per person
         //append this per person amount to the corresponding div
 }
-calculate();
