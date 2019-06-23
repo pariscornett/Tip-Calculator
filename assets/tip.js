921a8c78b107a25444ddf4a8f1cc99d31f09ddbd
Paris Cornett <pariscornett@gmail.com>
2019-06-22 22:52:05 -0500

    coded total bill plus tip and appended all calculations to DOM :brain:

--- assets/tip.js
+++ assets/tip.js
@@ -12,7 +12,7 @@ $("#submit").on("click" ,function(){
     event.preventDefault();
     let billInput = parseFloat($("#bill-input").val().trim());
     let tipInput = parseFloat($("#tip-input").val().trim()/100); //divided by 100 to turn tip input into percentage
-    let splitInput = $("#split-input").val().trim();
+    let splitInput = parseFloat($("#split-input").val().trim());
     $("#bill-input").val("");
     $("#tip-input").val("");
     $("#split-input").val("");
@@ -24,9 +24,12 @@ $("#submit").on("click" ,function(){
     let calculatedTip = billInput * tipInput;
     console.log("This is the calculated tip: " + calculatedTip);
     //append the tip amount to the corresponding div
-    //take the total bill amount and add the tip amount
-    //the total is the total amount, store this in a variable
+    $("#tip-display").append("$"+calculatedTip);
+    //take the total bill amount and add the tip amount, store this in a variable
+    let calculatedTotal = calculatedTip + billInput;
+    console.log( "This is the calculated total: " + calculatedTotal);
     //append the total amount to the corresponding div
+    $("#total-display").append("$"+calculatedTotal);
     //if the bill is being split more than 1 way,
         //divide the total amount by the split number
         //this is the amount per person
