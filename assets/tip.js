1c11082a97b967e27be27f118b19bd16cc564849
Paris Cornett <pariscornett@gmail.com>
2019-06-22 21:08:24 -0500

    added pseudocode :memo: :pencil:

---
+++ assets/tip.js
@@ -0,0 +1,17 @@
+//global variables go here
+
+//get input from form and store data in corresponding variables
+
+//function to calculate tip
+    //take the total bill amount
+    //take the desired tip amount and convert this to a decimal (move decimal point two spaces to the left)
+    //multiply the total bill by the tip amount
+    //the total is the tip amount, store this in a variable
+    //append the tip amount to the corresponding div
+    //take the total bill amount and add the tip amount
+    //the total is the total amount, store this in a variable
+    //append the total amount to the corresponding div
+    //if the bill is being split more than 1 way,
+        //divide the total amount by the split number
+        //this is the amount per person
+        //append this per person amount to the corresponding div
