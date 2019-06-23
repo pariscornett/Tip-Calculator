4966b4a0f63f5f2e70e61a363ec041226aceb592
Paris Cornett <pariscornett@gmail.com>
2019-06-22 22:46:03 -0500

    coded tip calculation :sunglasses:

--- assets/tip.js
+++ assets/tip.js
@@ -10,8 +10,8 @@ let calculatedSplit;
 //get input from form and store data in corresponding variables
 $("#submit").on("click" ,function(){
     event.preventDefault();
-    let billInput = $("#bill-input").val().trim();
-    let tipInput = $("#tip-input").val().trim()/100; //divided by 100 to turn tip input into percentage
+    let billInput = parseFloat($("#bill-input").val().trim());
+    let tipInput = parseFloat($("#tip-input").val().trim()/100); //divided by 100 to turn tip input into percentage
     let splitInput = $("#split-input").val().trim();
     $("#bill-input").val("");
     $("#tip-input").val("");
@@ -19,15 +19,10 @@ $("#submit").on("click" ,function(){
     console.log(billInput);
     console.log(tipInput);
     console.log(splitInput);
-    calculate();
-   
-});
-
-//function to calculate tip
-function calculate(str){ 
-   //multiply the total bill by the tip amount,the total is the tip amount, store this in a variable
-   let calculatedTip = Number.parseFloat(billInput)* Number.parseFloat(tipInput);
-   console.log("This is the calculated tip: " + calculatedTip);
+    //now for calculations
+    //multiply the total bill by the tip amount,the total is the tip amount, store this in a variable
+    let calculatedTip = billInput * tipInput;
+    console.log("This is the calculated tip: " + calculatedTip);
     //append the tip amount to the corresponding div
     //take the total bill amount and add the tip amount
     //the total is the total amount, store this in a variable
@@ -36,4 +31,8 @@ function calculate(str){
         //divide the total amount by the split number
         //this is the amount per person
         //append this per person amount to the corresponding div
-}
+});
+
+   
+   
+ 
